e5fac1f7fe8a90d7647928337ea74a42a528ebd4
QuantoProgrammer <56931889+QuantoProgrammer@users.noreply.github.com>
2020-04-17 12:16:54 +0530

    Update Python Tab Opener.py

--- Python Tab Opener.py
+++ Python Tab Opener.py
@@ -12,45 +12,49 @@ def windowOpener(Title):
 
 	#opens a window with the given title and dimensions
 	
-	lbl = Label(window, text='Hello,what is your name?')
-	lbl.grid(column=0, row=0)
+	txt = Entry(window, width=70)
+	txt.grid(column=0, row=1)
 	
-	txt = Entry(window, width=20)
-	txt.grid(column=1, row=0)
+	Button(window, text = 'Search', bg = 'yellow', fg = 'blue')
+	btn.grid(column=1, row=1)
 	
-	def clicked():
-		Name = "Hello," + txt.get() + ". Click options to explore more."
-		lbl.configure(text = Name)
-	
-	btn = Button(window, text = 'Click Here', command = clicked(), bg = 'yellow', fg = 'blue')
-	btn.grid(column=2, row=0)
-	
-	#asks for your name along with button
+	#opens a search box
 	
 	menu = Menu(window)
 	
-	opt = Menu(menu)
-	opt.add_command(label='New Tab')
-	opt.add_command(label='Open...')
-	opt.add_command(label='Recent Files')
-	opt.add_separator()
-	opt.add_command(label='Save')
-	opt.add_command(label='Save as')
-	opt.add_separator()
-	opt.add_command(label='Undo')
-	opt.add_command(label='Redo')
-	opt.add_separator()
-	opt.add_command(label='Cut')
-	opt.add_command(label='Copy')
-	opt.add_command(label='Paste')
-	opt.add_command(label='Select all')
-	opt.add_separator()
-	opt.add_command(label='Close')
-	opt.add_command(label='Exit Window')
-	opt.add_separator()
-	opt.add_command(label='Help')
-	opt.add_cascade(
+	options = Menu(menu)
 	
+	options.add_command(label='New Tab')
+	options.add_command(label='Open...')
+	options.add_command(label='Recent Files')
+	
+	options.add_separator()
+	
+	options.add_command(label='Save')
+	options.add_command(label='Save as')
+	
+	options.add_separator()
+	
+	options.add_command(label='Undo')
+	options.add_command(label='Redo')
+	
+	options.add_separator()
+	
+	options.add_command(label='Cut')
+	options.add_command(label='Copy')
+	options.add_command(label='Paste')
+	options.add_command(label='Select all')
+	
+	options.add_separator()
+	
+	options.add_command(label='Close')
+	options.add_command(label='Exit Window')
+	
+	options.add_separator()
+	
+	options.add_command(label='Help')
+	
+	options.add_cascade(label='Options' , menu = options)
 	window.config(menu=menu)
 	#opens a menu with options
 	
